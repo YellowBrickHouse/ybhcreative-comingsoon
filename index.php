80aa2f625b30158ae7b7a6ed80ba0a7491cee82f
cadie <cadie.brown@gmail.com>
2018-04-22 22:25:27 -0400

    Create first version of the site

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Yellow Brick House Creative</title>
+		<meta name="description" content="Yellow Brick House Creative">
+		<link rel="stylesheet" href="./static/css/style.css">
+	</head>
+	<body>
+		<div class="left-side">
+			<h1>Yellow Brick House</h1>
+			<h2>Creative</h2>
+			<p>Our site is currently under construction.</p>
+			<p>You can check back here on July 1st, or if you're interested in working with us feel free to <a href="mailto:bryce@ybhcreative.com">send us an email</a>.</p>
+		</div>
+		<div class="right-side">
+			<img src="static/imgs/ybhcreative-comingsoon.jpg" alt="Yellow Brick House Creative">
+		</div>
+	</body>
+</html>
